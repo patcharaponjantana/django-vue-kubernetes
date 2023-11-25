d4ca4aab2835e6c52113c7882e77ac66e0a76ba7
Patcharapon Jantana <patcharaponjantana@gmail.com>
2023-11-25 20:36:08 +0700

    [update] setting

--- backend/train_booking/settings.py
+++ backend/train_booking/settings.py
@@ -76,12 +76,17 @@ WSGI_APPLICATION = 'train_booking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', None),
-        'USER': os.environ.get('POSTGRES_USER', None),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', None),
-        'HOST': os.environ.get('DB_HOST', None),
-        'PORT': os.environ.get('DB_PORT', None),
+        # for postgresql
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.environ.get('POSTGRES_DB', None),
+        # 'USER': os.environ.get('POSTGRES_USER', None),
+        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD', None),
+        # 'HOST': os.environ.get('DB_HOST', None),
+        # 'PORT': os.environ.get('DB_PORT', None),
+
+        # for sqlite
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 
 }
